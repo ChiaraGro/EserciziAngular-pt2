cf9cd1e70cc42d68cb516ceb76d4783fb4fb70e7
Chiara Grottini <chiaragrottini@gmail.com>
2021-12-01 16:07:41 +0100

    implemented components

---
+++ src/app/show-counter/show-counter.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CounterService } from '../counter.service';
+
+@Component({
+  selector: 'app-show-counter',
+  templateUrl: './show-counter.component.html',
+  styleUrls: ['./show-counter.component.css']
+})
+export class ShowCounterComponent implements OnInit {
+
+  constructor(public counterService: CounterService) {
+  }
+
+  counter = this.counterService.getValue();
+
+  ngOnInit(): void {
+
+  }
+
+}
