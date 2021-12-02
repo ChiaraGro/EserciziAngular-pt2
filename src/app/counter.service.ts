df40f7ffb1b77303208d559add364b374519cf59
Chiara Grottini <chiaragrottini@gmail.com>
2021-12-02 11:37:44 +0100

    added style

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class CounterService {
 
-  counter : number = 0;
+  private counter : number = 0;
   value: number = 1;
 
   constructor() { }
