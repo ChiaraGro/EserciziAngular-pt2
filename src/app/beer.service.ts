d3962251c08cc28532bf54db05421cda6f73fac7
Chiara Grottini <chiaragrottini@gmail.com>
2021-12-07 10:57:54 +0100

    add observable

--- src/app/beer.service.ts
+++ src/app/beer.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { IBeer } from './models/beer';
+import { BEERS } from './models/mock-beers';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BeerService {
+  beers: IBeer[] = BEERS; //da tipizzare?
+  beers$ = new BehaviorSubject<IBeer[]>(this.beers);
+
+
+
 
   constructor() { }
+
+  getBeers(): Observable<IBeer[]>{
+    return this.beers$.asObservable();
+  }
+
 }
