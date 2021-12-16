278e260851c217f6ef43c74ed054ae7317131a99
Chiara Grottini <chiaragrottini@gmail.com>
2021-12-16 19:04:44 +0100

    add endpoints

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -11,11 +11,19 @@ export class ContainerComponent implements OnInit {
   constructor(private covidService: CovidService) { }
 
   properties: string[] = [];
+  endpoints: string[] = [];
 
   ngOnInit(): void {
-    this.covidService.getAll().subscribe(data => console.log(data));
+    this.covidService.getAll().subscribe(data =>
+      {for(let el of Object.values(data))
+        {console.log(el.Path);
+        this.endpoints = el.Path}
 
-    this.covidService.getAll().subscribe(data => this.properties = Object.keys(data) )
+     }
+      );
+
+    this.covidService.getAll().subscribe(data => this.properties = Object.keys(data))
+    this.covidService.getAll().subscribe(data => this.endpoints = Object.values(data))
   }
 
 }
