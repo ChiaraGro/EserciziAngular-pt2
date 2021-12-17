9b0f9a1ee2196e368d077039256e82579f0c41d5
Chiara Grottini <chiaragrottini@gmail.com>
2021-12-17 16:33:28 +0100

    add interface

--- src/app/country.ts
+++ src/app/country.ts
@@ -4,27 +4,3 @@
 //   Path: string;
 // }
 
-// export interface ICountries {
-//   countriesRoute: ICountryRoute;
-//   countryDayOneRoute: ICountryRoute;
-//   countryDayOneTotalRoute: ICountryRoute;
-//   countryRoute: ICountryRoute;
-//   countryRoutePremium: ICountryRoute;
-//   countryRoutePremiumData: ICountryRoute;
-//   countryStatusDayOneLiveRoute: ICountryRoute;
-//   countryStatusDayOneRoute: ICountryRoute;
-//   countryStatusDayOneTotalRoute: ICountryRoute;
-//   countryStatusLiveRoute: ICountryRoute;
-//   countryStatusRoute: ICountryRoute;
-//   countryStatusTotalRoute: ICountryRoute;
-//   countrySummaryRoutePremium: ICountryRoute;
-//   countryTestingPremium: ICountryRoute;
-//   countryTotalRoute: ICountryRoute;
-//   exportRoute: ICountryRoute;
-//   liveCountryRoute: ICountryRoute;
-//   liveCountryStatusAfterDateRoute: ICountryRoute;
-//   liveCountryStatusRoute: ICountryRoute;
-//   summaryRoute: ICountryRoute;
-//   travelAdvicePremium: ICountryRoute;
-//   webhookRoute: ICountryRoute;
-// }
